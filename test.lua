3172e4351106a494e4d393025e48ab8d1b284ef1
TheJellyfish1412 <152600168+TheJellyfish1412@users.noreply.github.com>
2024-09-21 00:31:12 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,51 +1,83 @@
+local Combat = game:GetService("ReplicatedStorage").Events.Combat
 local LocalPlayer = game.Players.LocalPlayer
-if not _G.part then
-  _G.part = Instance.new("Part")
-end
-_G.part.Size = Vector3.new(50, 1, 50)
-_G.part.Anchored = true
-_G.part.Transparency = 0
-_G.part.Parent = game.Workspace
+local HumanoidRootPart = LocalPlayer.Character.HumanoidRootPart
 
-local LocalPlayer = game.Players.LocalPlayer
-if not _G.part then
-  _G.part = Instance.new("Part")
-end
-_G.part.Size = Vector3.new(50, 1, 50)
-_G.part.Anchored = true
-_G.part.Transparency = 0
-_G.part.Parent = game.Workspace
 
-local LocalPlayer = game.Players.LocalPlayer
-local Object = {
-  ["OakTree"] = 3,
-  -- ["Rock"] = 3,
-  -- ["PalmTree"]  = 3,
-}
-function getMesh(folder)
-  for i,v in pairs(folder:GetDescendants()) do
-    if v.ClassName == "MeshPart" then
-      return v
+function getPlayerHp()
+  for i,v in pairs(LocalPlayer.PlayerGui.Actions.PartyMemberUI.Frame:GetChildren()) then
+    if v.ClassName == "Frame" then
+      local target = v.Frame.PlayerStats.PlayerName.Text
+      local healthText = v.Frame.PlayerStats.HPFrame.TextFrame.AmountText.Text
+      local num1, num2 = healthText:match("(%d+)/(%d+)")
+      num1, num2 = tonumber(num1), tonumber(num2)
+
+      return {target , (num2/num1)*100}
     end
   end
 end
-for name, cd in pairs(Object) do
-  for _,v in pairs(game:GetService("Workspace").Game.Harvest[name]:GetChildren()) do
-    if v.Name == "HarvestModel" then
-      pcall(function()
-        local ModelParts = getMesh(v.ModelParts)
-        local PrimaryPart = v.PrimaryPart
-        if ModelParts.Transparency == 0 then
-          _G.part.CFrame = PrimaryPart.CFrame + Vector3.new(0, -5, 0)
-          LocalPlayer.Character.HumanoidRootPart.CFrame = PrimaryPart.CFrame + Vector3.new(0, 2, 0)
-          repeat wait()
-            fireproximityprompt(PrimaryPart.ProximityPrompt)
-          until ModelParts.Transparency == 1
-          wait(cd)
-        end
-      end)
+
+function follow()
+  local func = {}
+
+  function func:createPath()
+    if not _G.part then
+      _G.part = Instance.new("Part")
     end
+    _G.part.Size = Vector3.new(50, 1, 50)
+    _G.part.Anchored = true
+    _G.part.Transparency = 0
+    _G.part.Parent = game.Workspace
   end
+
+  function func:Tp()
+    spawn(function()
+      local target = game.Players[_G.target].Character.HumanoidRootPart
+      while _G.follow do
+        task.wait()
+        _G.part.CFrame = target.CFrame + Vector3.new(0, -27, 0)
+        HumanoidRootPart.CFrame = target.CFrame + Vector3.new(0, -25, 0)
+      end
+    end)
+  end
+
+  return func
 end
-LocalPlayer.Character.HumanoidRootPart.CFrame = LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, 2, 0)
-LocalPlayer.Character.HumanoidRootPart.CFrame = LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, 2, 0)
+
+local MainTarget = "Phruetsakol"
+_G.follow = true
+_G.AutoSkill = true
+_G.target = MainTarget
+
+local MyFollow = follow()
+MyFollow:createPath()
+MyFollow:Tp()
+
+while _G.AutoSkill do
+  task.wait()
+  local target, hp = getPlayerHp()
+  if (hp < 80) then
+    local args = {
+      [1] = "HealShot",
+      [2] = HumanoidRootPart.CFrame,
+      [3] = game.Players[target].Character.HumanoidRootPart.Position
+    }
+    _G.target = target
+    wait(.5)
+    Combat.ActivateSkill:FireServer(unpack(args))
+  end
+
+  local target, hp = getPlayerHp()
+  if (hp < 80) then
+    local args = {
+      [1] = "GroupHeal",
+      [2] = HumanoidRootPart.CFrame,
+      [3] = game.Players[target].Character.HumanoidRootPart.Position
+    }
+    _G.target = target
+    wait(.5)
+    Combat.ActivateSkill:FireServer(unpack(args))
+  end
+
+  wait(3)
+  _G.target = MainTarget
+end
