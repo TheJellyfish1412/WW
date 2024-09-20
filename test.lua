134c8898ea6f848da91fea58af0b703ee3b16330
TheJellyfish1412 <152600168+TheJellyfish1412@users.noreply.github.com>
2024-09-20 18:10:19 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -8,8 +8,17 @@ _G.part.Transparency = 0
 _G.part.Parent = game.Workspace
 
 local LocalPlayer = game.Players.LocalPlayer
+if not _G.part then
+  _G.part = Instance.new("Part")
+end
+_G.part.Size = Vector3.new(50, 1, 50)
+_G.part.Anchored = true
+_G.part.Transparency = 0
+_G.part.Parent = game.Workspace
+
+local LocalPlayer = game.Players.LocalPlayer
 local Object = {
-  ["OakTree"] = 4,
+  ["OakTree"] = 3,
   -- ["Rock"] = 3,
   -- ["PalmTree"]  = 3,
 }
@@ -20,8 +29,8 @@ function getMesh(folder)
     end
   end
 end
-for _, name in pairs(Object) do
-  for v,cd in pairs(game:GetService("Workspace").Game.Harvest[name]:GetChildren()) do
+for name, cd in pairs(Object) do
+  for _,v in pairs(game:GetService("Workspace").Game.Harvest[name]:GetChildren()) do
     if v.Name == "HarvestModel" then
       pcall(function()
         local ModelParts = getMesh(v.ModelParts)
@@ -38,4 +47,5 @@ for _, name in pairs(Object) do
     end
   end
 end
+LocalPlayer.Character.HumanoidRootPart.CFrame = LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, 2, 0)
 LocalPlayer.Character.HumanoidRootPart.CFrame = LocalPlayer.Character.HumanoidRootPart.CFrame + Vector3.new(0, 2, 0)
