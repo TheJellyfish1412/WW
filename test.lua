0ed5858726234f6f3bfa5a6cd7b082847b372ef6
TheJellyfish1412 <152600168+TheJellyfish1412@users.noreply.github.com>
2024-10-01 21:34:47 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,82 +1,70 @@
-local Combat = game:GetService("ReplicatedStorage").Events.Combat
-local LocalPlayer = game.Players.LocalPlayer
-local HumanoidRootPart = LocalPlayer.Character.HumanoidRootPart
+local success, errorMessage = pcall(function()
 
+  local LocalPlayer = game.Players.LocalPlayer
+  local HumanoidRootPart = LocalPlayer.Character.HumanoidRootPart
+  local Combat = game:GetService("ReplicatedStorage").Events.Combat
 
-function getPlayerHp()
-  for i,v in pairs(LocalPlayer.PlayerGui.Actions.PartyMemberUI.Frame:GetChildren()) then
-    if v.ClassName == "Frame" then
-      local target = v.Frame.PlayerStats.PlayerName.Text
-      local healthText = v.Frame.PlayerStats.HPFrame.TextFrame.AmountText.Text
-      local num1, num2 = healthText:match("(%d+)/(%d+)")
-      num1, num2 = tonumber(num1), tonumber(num2)
-
-      return {target , (num2/num1)*100}
-    end
+  if not _G.part then
+    _G.part = Instance.new("Part")
   end
-end
+  _G.part.Size = Vector3.new(50, 1, 50)
+  _G.part.Anchored = true
+  _G.part.Transparency = 0
+  _G.part.Parent = game.Workspace
 
-function follow()
-  local func = {}
 
-  function func:createPath()
-    if not _G.part then
-      _G.part = Instance.new("Part")
-    end
-    _G.part.Size = Vector3.new(50, 1, 50)
-    _G.part.Anchored = true
-    _G.part.Transparency = 0
-    _G.part.Parent = game.Workspace
-  end
+  local list = {
+    [15] = game:GetService("Workspace").Game.Regions.Dion.Areas.AncientRuins.MobsSpots.FeralFiendSoldier,
+    [10] = game:GetService("Workspace").Game.Regions.Dion.Areas.AncientRuins.MobsSpots.FeralFiendGeneral
+  }
 
-  function func:Tp()
-    spawn(function()
-      while _G.follow do
+  _G.AutoFarm = true
+  while (_G.AutoFarm) do
+    wait()
+    for num, mobPath in pairs(list) do
+      if not _G.AutoFarm then break end
+      wait()
+      count = 0
+      for _,mob in pairs(mobPath:GetChildren()) do
+        if not _G.AutoFarm then break end
         task.wait()
-        local target = game.Players[_G.target].Character.HumanoidRootPart
-        _G.part.CFrame = target.CFrame + Vector3.new(0, -27, 0)
-        HumanoidRootPart.CFrame = target.CFrame + Vector3.new(0, -25, 0)
+        local HP = mob.UndeadBear.Head.HealthAndMana.PlayerBars.Bars.HPFrame.CanvasGroup.TextFrame.AmountText
+        local BodyHitbox = mob.UndeadBear.BodyHitbox
+        local MobPart = mob.UndeadBear.HumanoidRootPart
+        
+        local cframe = CFrame.new(MobPart.Position)
+        _G.part.CFrame = cframe + Vector3.new(0, -27, 0)
+        HumanoidRootPart.CFrame = cframe + Vector3.new(0, -25, 0)
+        
+        BodyHitbox.Size = Vector3.new(60, 60, 60)
+        -- LocalPlayer.Character.RightHand.RightGrip.Part1 = nil
+        LocalPlayer.Character.Weapon.Handle.Handle.Part1 = nil
+        wait()
+        LocalPlayer.Character.Weapon.Handle.Anchored = true
+        LocalPlayer.Character.Weapon.Handle.FirePoint.Position = Vector3.new(0,0,0)
+
+        if _G.AutoFarm then
+          if (tonumber(HP.Text) > 0) then
+            count = count + 1
+          end
+          while tonumber(HP.Text) > 0 and _G.AutoFarm do
+            task.wait()
+            _G.Click()
+            HumanoidRootPart.CFrame = cframe + Vector3.new(0, -25, 0)
+            LocalPlayer.Character.Weapon.Handle.Position = BodyHitbox.Position
+            _G.PosMon = BodyHitbox.Position
+            -- BodyHitbox.CFrame = HumanoidRootPart.CFrame
+          end
+        end
+        if (count >= num) then
+          break
+        end
       end
-    end)
+    end
   end
+end)
 
-  return func
-end
-
-local MainTarget = "Phruetsakol"
-_G.follow = true
-_G.AutoSkill = true
-_G.target = MainTarget
-
-local MyFollow = follow()
-MyFollow:createPath()
-MyFollow:Tp()
-
-while _G.AutoSkill do
-  task.wait()
-  local target, hp = getPlayerHp()
-  _G.target = target
-
-  if (hp < 80) then
-    local args = {
-      [1] = "HealShot",
-      [2] = HumanoidRootPart.CFrame,
-      [3] = game.Players[target].Character.HumanoidRootPart.Position
-    }
-    wait(.5)
-    Combat.ActivateSkill:FireServer(unpack(args))
-  end
-
-  if (hp < 80) then
-    local args = {
-      [1] = "GroupHeal",
-      [2] = HumanoidRootPart.CFrame,
-      [3] = game.Players[target].Character.HumanoidRootPart.Position
-    }
-    wait(.5)
-    Combat.ActivateSkill:FireServer(unpack(args))
-  end
-
-  wait(3)
-  _G.target = MainTarget
+if not success then
+  local errorString = tostring(errorMessage)
+  toclipboard(errorString)
 end
