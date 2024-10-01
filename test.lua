8855a226f9f204ada2ebcf11f762eefc9219b6e2
TheJellyfish1412 <152600168+TheJellyfish1412@users.noreply.github.com>
2024-10-02 00:25:22 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,7 @@
 local success, errorMessage = pcall(function()
 
   local LocalPlayer = game.Players.LocalPlayer
+  local camera = workspace.CurrentCamera
   local HumanoidRootPart = LocalPlayer.Character.HumanoidRootPart
   local Combat = game:GetService("ReplicatedStorage").Events.Combat
 
@@ -12,10 +13,9 @@ local success, errorMessage = pcall(function()
   _G.part.Transparency = 0
   _G.part.Parent = game.Workspace
 
-
   local list = {
-    [15] = game:GetService("Workspace").Game.Regions.Dion.Areas.AncientRuins.MobsSpots.FeralFiendSoldier,
-    [10] = game:GetService("Workspace").Game.Regions.Dion.Areas.AncientRuins.MobsSpots.FeralFiendGeneral
+    [15] = "FeralFiendSoldier",
+    [10] = "FeralFiendGeneral"
   }
 
   _G.AutoFarm = true
@@ -25,7 +25,10 @@ local success, errorMessage = pcall(function()
       if not _G.AutoFarm then break end
       wait()
       count = 0
-      for _,mob in pairs(mobPath:GetChildren()) do
+      
+      wait(1)
+      game:GetService("ReplicatedStorage").Events.Quest.GrantQuest:FireServer("MarshFiendPurge")
+      for _,mob in pairs(game:GetService("Workspace").Game.Regions.Dion.Areas.AncientRuins.MobsSpots[mobPath]:GetChildren()) do
         if not _G.AutoFarm then break end
         task.wait()
         local HP = mob.UndeadBear.Head.HealthAndMana.PlayerBars.Bars.HPFrame.CanvasGroup.TextFrame.AmountText
@@ -36,9 +39,12 @@ local success, errorMessage = pcall(function()
         _G.part.CFrame = cframe + Vector3.new(0, -27, 0)
         HumanoidRootPart.CFrame = cframe + Vector3.new(0, -25, 0)
         
-        BodyHitbox.Size = Vector3.new(60, 60, 60)
-        -- LocalPlayer.Character.RightHand.RightGrip.Part1 = nil
-        LocalPlayer.Character.Weapon.Handle.Handle.Part1 = nil
+        BodyHitbox.Size = Vector3.new(20, 20, 20)
+        if (LocalPlayer.Character.RightHand:FindFirstChild("RightGrip")) then
+          LocalPlayer.Character.RightHand.RightGrip.Part1 = nil
+        else if (LocalPlayer.Character.Weapon.Handle:FindFirstChild("Handle")) then
+          LocalPlayer.Character.Weapon.Handle.Handle.Part1 = nil
+        end
         wait()
         LocalPlayer.Character.Weapon.Handle.Anchored = true
         LocalPlayer.Character.Weapon.Handle.FirePoint.Position = Vector3.new(0,0,0)
@@ -49,10 +55,13 @@ local success, errorMessage = pcall(function()
           end
           while tonumber(HP.Text) > 0 and _G.AutoFarm do
             task.wait()
-            _G.Click()
             HumanoidRootPart.CFrame = cframe + Vector3.new(0, -25, 0)
             LocalPlayer.Character.Weapon.Handle.Position = BodyHitbox.Position
-            _G.PosMon = BodyHitbox.Position
+
+            local cameraPosition = camera.CFrame.Position
+            camera.CFrame = CFrame.new(cameraPosition, BodyHitbox.Position)
+
+            _G.Click()
             -- BodyHitbox.CFrame = HumanoidRootPart.CFrame
           end
         end
@@ -67,4 +76,7 @@ end)
 if not success then
   local errorString = tostring(errorMessage)
   toclipboard(errorString)
+else 
+  toclipboard("Pass")
 end
+
